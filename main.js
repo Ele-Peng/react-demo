9613f686e778bebc89b3c197a9ed7ee7e00025aa
Ele-Peng <374237925@qq.com>
2020-11-29 15:39:03 +0800

    feat: add setState

--- main.js
+++ main.js
@@ -10,7 +10,13 @@ class MyComponent extends Component{
   render() {
     return <div>
         <h1>My Component</h1>
-        <span>{this.state.a.toString()}</span>
+        <button onclick={() => {
+          this.setState({
+            a: this.state.a + 1
+          })
+        }}>add</button>
+        <div>{this.state.a.toString()}</div>
+        <div>{this.state.b.toString()}</div>
         {this.children}
       </div>
   }
