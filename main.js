c3d2872538a164258f19d9195cddeab5aff0507c
Ele-Peng <374237925@qq.com>
2020-11-29 15:10:46 +0800

    update: range to root

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import { createElement, Component, render } from './toy-react.js';
 class MyComponent extends Component{
+  constructor() {
+    super();
+    this.state = {
+      a: 12333,
+      b: 2
+    }
+  }
   render() {
     return <div>
         <h1>My Component</h1>
+        <span>{this.state.a.toString()}</span>
         {this.children}
       </div>
   }
